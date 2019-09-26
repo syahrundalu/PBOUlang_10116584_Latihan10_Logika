15d20ef58dedf3e9c57b22254b0a1394b605b366
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:54:10 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan10_Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan10_Logika;
+
+/**
+ *
+ * @author 
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa : "+ dewasa);
+    }
+    
+}
